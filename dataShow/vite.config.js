9d76aa20ec3c00862a6f957937e2196ce927434f
Luis Rondon <luisrondon2605@yahoo.com>
2023-09-12 13:30:19 +0200

    test2-4

--- dataShow/vite.config.js
+++ dataShow/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       manifest: {
         name: "dataShow",
         short_name: "dShow",
-        start_url: "https://luis2605.github.io/dataShow/",
+        start_url: "/dataShow/",
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#000000",
